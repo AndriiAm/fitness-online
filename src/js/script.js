ccf7ed8bd59a52011ac6c1f6c01b70c89ba986f1
andrii amelchenko <amelchenkoan29@gmail.com>
2024-04-27 03:12:11 +0300

    final

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+import menu from "./modules/menu";
+import swiper from "./modules/swiper";
+import video from "./modules/video";
+import accordion from "./modules/accordion";
+
+menu();
+swiper();
+video();
+accordion();
